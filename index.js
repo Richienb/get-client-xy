1e85a098da916e8ecace57c62a6c0ec3209112cd
Dmitry <df.creative@gmail.com>
2015-07-05 03:46:36 +0300

    Expose findTouch proper method

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function getClientY (e, idx) {
 	// touch event
 	if (e.touches) {
 		if (arguments.length > 1) {
-			return findTouch(e, idx).clientY
+			return findTouch(e.touches, idx).clientY
 		}
 		else {
 			return e.targetTouches[0].clientY;
@@ -27,7 +27,7 @@ function getClientX (e, idx) {
 	// touch event
 	if (e.touches) {
 		if (arguments.length > idx) {
-			return findTouch(e, idx).clientX;
+			return findTouch(e.touches, idx).clientX;
 		}
 		else {
 			return e.targetTouches[0].clientX;
@@ -42,12 +42,10 @@ function getClientXY (e, idx) {
 	return [getClientX.apply(this, arguments), getClientY.apply(this, arguments)];
 }
 
-function findTouch (e, idx) {
-	var touches = e.touches;
-
-	for (var i = 0; i < touches.length; i++) {
-		if (touches[i].identifier === idx) {
-			return touches[i];
+function findTouch (touchList, idx) {
+	for (var i = 0; i < touchList.length; i++) {
+		if (touchList[i].identifier === idx) {
+			return touchList[i];
 		}
 	}
 }
